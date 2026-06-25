256ae18926e0d4a9c424ffe0fa7ddec87db55341
DaviKM <davikmach@gmail.com>
2026-06-25 11:56:26 -0300

    Initialize dotenv and set up DATABASE_URL in main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {"Hello":"World"}
+
+@app.get('/adicionar-cliente')
+def 
